954246c3ef30bef84fd21aed563150b6524c51ef
Mohamed Hesham <medo93346@gmail.com>
2025-04-29 02:24:26 +0300

    init project

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,13 @@ repositories {
 }
 
 dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.6.2")
     testImplementation(kotlin("test"))
+    implementation("io.insert-koin:koin-core:4.0.2")
+    // tests
+    testImplementation("com.google.truth:truth:1.4.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.12.1")
+    testImplementation("io.mockk:mockk:1.13.16")
 }
 
 tasks.test {
