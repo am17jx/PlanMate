07bc7508f30734ca91eeb7c6a7f46a6b58ccc38b
edrees <muhammed.edrees101@gmail.com>
2025-04-30 00:29:07 +0300

    test: add test cases for CSVWriter

--- src/main/kotlin/data/utils/CSVWriter.kt
+++ src/main/kotlin/data/utils/CSVWriter.kt
@@ -7,9 +7,14 @@ import java.io.FileWriter
 import java.io.IOException
 
 class CSVWriter(
-    private val filePath: String,
+    private val file: File,
 ) {
     fun writeLines(lines: List<String>, append: Boolean = false) {
         TODO("Not yet implemented")
+    }
+    companion object{
+        const val CANNOT_WRITE_TO_FILE_ERROR_MESSAGE = "Cannot write to CSV file"
+        const val DIRECTORY_INSTEAD_OF_FILE_ERROR_MESSAGE = "Expected a file but got a directory"
+        const val EMPTY_LINES_ERROR_MESSAGE = "Cannot write empty lines to CSV file"
     }
 }
