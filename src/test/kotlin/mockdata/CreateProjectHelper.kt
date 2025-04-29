1cb4b5b6d47bdf72d1ec401be65f292f1e1a79e5
edrees <muhammed.edrees101@gmail.com>
2025-04-29 19:39:21 +0300

    test: add helper functions for creating different objects

---
+++ src/test/kotlin/mockdata/CreateProjectHelper.kt
@@ -0,0 +1,24 @@
+package mockdata
+
+import org.example.logic.models.Project
+import org.example.logic.models.State
+
+fun createProject(
+    id: String = "",
+    name: String = "",
+    states: List<State> = emptyList(),
+    auditLogsIds: List<String> = emptyList()
+) = Project(
+    id = id,
+    name = name,
+    states = states,
+    auditLogsIds = auditLogsIds
+)
+
+fun createState(
+    id: String = "",
+    title: String = ""
+) = State(
+    id = id,
+    title = title
+)
