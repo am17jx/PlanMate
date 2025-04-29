2dee5d9ad79a9277bba2f5e14b8d29bffad8b513
Abdallah Bakhet <abdallahM_bakhet@outlook.com>
2025-04-29 20:33:54 +0300

    test: add tests for permission and user checks

--- src/test/kotlin/logic/useCase/CreateProjectUseCaseTest.kt
+++ src/test/kotlin/logic/useCase/CreateProjectUseCaseTest.kt
@@ -1,12 +1,17 @@
 package logic.useCase
 
 import com.google.common.truth.Truth.assertThat
+import io.mockk.every
 import io.mockk.mockk
+import org.example.logic.models.User
+import org.example.logic.models.UserRole
 import org.example.logic.repositries.AuditLogRepository
 import org.example.logic.repositries.AuthenticationRepository
 import org.example.logic.repositries.ProjectRepository
 import org.example.logic.useCase.CreateProjectUseCase
 import org.example.logic.utils.BlankInputException
+import org.example.logic.utils.InsufficientPermissionsException
+import org.example.logic.utils.NoLoggedInUserException
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
@@ -40,6 +45,26 @@ class CreateProjectUseCaseTest {
     }
 
     @Test
+    fun `should throw InsufficientPermissionsException when user is not an admin`() {
+        val projectName = "Test Project"
+        every { authenticationRepository.getCurrentUser() } returns User("", "", "", UserRole.USER)
+
+        assertThrows<InsufficientPermissionsException> {
+            createProjectUseCase(projectName)
+        }
+    }
+
+    @Test
+    fun `should throw NoLoggedInUserException when current user is null`() {
+        val projectName = "Test Project"
+        every { authenticationRepository.getCurrentUser() } returns null
+
+        assertThrows<NoLoggedInUserException> {
+            createProjectUseCase(projectName)
+        }
+    }
+
+    @Test
     fun `should return project with the same given name when project created successfully`() {
         val projectName = "Test Project"
 
@@ -54,15 +79,15 @@ class CreateProjectUseCaseTest {
 
         val result = createProjectUseCase(projectName)
 
-        assertThat(result.auditLogsIds).isEmpty()
+        assertThat(result.states).isEmpty()
     }
 
     @Test
-    fun `should return project with empty list of auditLogsIds when project created successfully`() {
+    fun `should return project with one item in auditLogsIds when project created successfully`() {
         val projectName = "Test Project"
 
         val result = createProjectUseCase(projectName)
 
-        assertThat(result.auditLogsIds).isEmpty()
+        assertThat(result.auditLogsIds.size).isEqualTo(1)
     }
 }
