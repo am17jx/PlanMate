d3bc954b4d08b605dec852c4cacdc06d6c88a1de
Khaled.Eid <khaled.eid1k1k@gmail.com>
2025-04-29 20:35:35 +0200

    feat: add GetAllProjectsUseCase

--- src/main/kotlin/logic/repositries/ProjectRepository.kt
+++ src/main/kotlin/logic/repositries/ProjectRepository.kt
@@ -6,6 +6,6 @@ interface ProjectRepository {
     fun createProject(project: Project): Project
     fun updateProject(updatedProject: Project): Project
     fun deleteProject(projectId: String)
-    fun getAllProjects(): List<Project>
+    fun getAllProjects(): List<Project>?
     fun getProjectById(projectId: String): Project
 }
