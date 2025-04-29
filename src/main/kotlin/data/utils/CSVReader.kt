6ec29bb6b1d668cc6340f847dfc5a71f0fdf39f8
edrees <muhammed.edrees101@gmail.com>
2025-04-30 00:28:48 +0300

    test: add test cases for CSVReader

--- src/main/kotlin/data/utils/CSVReader.kt
+++ src/main/kotlin/data/utils/CSVReader.kt
@@ -6,9 +6,14 @@ import java.io.FileNotFoundException
 import java.io.IOException
 
 class CSVReader(
-    private val filePath: String,
+    private val file: File,
 ) {
     fun readLines(): List<String> {
         TODO("not yet implemented")
+    }
+    companion object{
+        const val FILE_NOT_FOUND_ERROR_MESSAGE = "CSV file not found"
+        const val CANNOT_READ_FILE_ERROR_MESSAGE = "Cannot read from CSV file"
+        const val DIRECTORY_INSTEAD_OF_FILE_ERROR_MESSAGE = "Expected a file but got a directory"
     }
 }
