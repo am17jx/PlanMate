5e5bd5a14fc4ab3eb9cbf98556cab5c49678e5b7
edrees <muhammed.edrees101@gmail.com>
2025-04-29 23:36:42 +0300

    feat: initialize CSVReader and CSVWriter

---
+++ src/main/kotlin/data/utils/CSVReader.kt
@@ -0,0 +1,14 @@
+package org.example.data.utils
+
+import java.io.BufferedReader
+import java.io.File
+import java.io.FileNotFoundException
+import java.io.IOException
+
+class CSVReader(
+    private val filePath: String,
+) {
+    fun readLines(): List<String> {
+        TODO("not yet implemented")
+    }
+}
---
+++ src/main/kotlin/data/utils/CSVWriter.kt
@@ -0,0 +1,15 @@
+package org.example.data.utils
+
+import java.io.BufferedWriter
+import java.io.File
+import java.io.FileNotFoundException
+import java.io.FileWriter
+import java.io.IOException
+
+class CSVWriter(
+    private val filePath: String,
+) {
+    fun writeLines(lines: List<String>, append: Boolean = false) {
+        TODO("Not yet implemented")
+    }
+}
