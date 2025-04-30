d43e7b2227d87dd72ac6cd9b026a4b0f82cf27a5
Abdallah Bakhet <abdallahM_bakhet@outlook.com>
2025-05-01 02:50:32 +0300

    feat: implement delete state functionality

--- src/main/kotlin/logic/useCase/DeleteStateUseCase.kt
+++ src/main/kotlin/logic/useCase/DeleteStateUseCase.kt
@@ -1,9 +1,17 @@
 package org.example.logic.useCase
 
 import org.example.logic.models.Project
+import org.example.logic.models.State
+import org.example.logic.models.UserRole
 import org.example.logic.repositries.AuthenticationRepository
 import org.example.logic.repositries.ProjectRepository
+import org.example.logic.utils.BlankInputException
+import org.example.logic.utils.NoLoggedInUserException
+import org.example.logic.utils.ProjectNotFoundException
+import org.example.logic.utils.UnauthorizedException
+import kotlin.uuid.ExperimentalUuidApi
 
+@OptIn(ExperimentalUuidApi::class)
 class DeleteStateUseCase(
     private val projectRepository: ProjectRepository,
     private val authenticationRepository: AuthenticationRepository,
@@ -13,6 +21,43 @@ class DeleteStateUseCase(
         stateId: String,
         projectId: String,
     ): Project {
-        TODO("Not yet implemented")
+        checkInputValidation(stateId, projectId)
+        checkUserRole()
+        val project = getProject(projectId)
+        val updatedStates = removeState(project, stateId)
+        val updatedProject =
+            updateProjectUseCase(
+                project.copy(
+                    states = updatedStates,
+                ),
+            )
+        return updatedProject
     }
+
+    private fun removeState(
+        project: Project,
+        stateId: String,
+    ): List<State> = project.states.filter { it.id != stateId }
+
+    private fun getProject(projectId: String): Project =
+        projectRepository.getProjectById(projectId)
+            ?: throw ProjectNotFoundException("Project not found")
+
+    private fun checkUserRole() {
+        if (getCurrentUser().role != UserRole.ADMIN) throw UnauthorizedException("User is not an admin")
+    }
+
+    private fun checkInputValidation(
+        stateId: String,
+        projectId: String,
+    ) {
+        when {
+            stateId.isBlank() -> throw BlankInputException("State id cannot be blank")
+            projectId.isBlank() -> throw BlankInputException("Project id cannot be blank")
+        }
+    }
+
+    private fun getCurrentUser() =
+        authenticationRepository.getCurrentUser()
+            ?: throw NoLoggedInUserException("No logged in user")
 }
