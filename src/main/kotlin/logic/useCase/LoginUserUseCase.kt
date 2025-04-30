2079e70e4bfe70a13dc2fe0f8b06cc44df6df848
Mostafa Salah <mostafa.salah.569x@gmail.com>
2025-04-30 14:27:52 +0300

    refactor: enhance function logic in LoginUserUseCase

--- src/main/kotlin/logic/useCase/LoginUserUseCase.kt
+++ src/main/kotlin/logic/useCase/LoginUserUseCase.kt
@@ -9,10 +9,13 @@ import org.example.logic.utils.hashWithMD5
 class LoginUserUseCase(private val authenticationRepository: AuthenticationRepository) {
 
     operator fun invoke(username: String, password: String): User {
-        val hashedPassword = hashWithMD5(password)
         when {
             username.isBlank() -> throw BlankInputException("Username is blank")
             password.isBlank() -> throw BlankInputException("Password is blank")
+        }
+
+        val hashedPassword = hashWithMD5(password)
+        when {
             isUserNotFound(username, hashedPassword) -> throw UserNotFoundException("User not found")
             else -> return authenticationRepository.login(username, hashedPassword)
         }
