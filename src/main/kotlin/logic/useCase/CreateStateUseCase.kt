b50f6949594b494fc9dcb19d7ea43a69ff05c5f0
Abdallah Bakhet <abdallahM_bakhet@outlook.com>
2025-05-01 02:43:35 +0300

    feat: implement create state functionality

--- src/main/kotlin/logic/useCase/CreateStateUseCase.kt
+++ src/main/kotlin/logic/useCase/CreateStateUseCase.kt
@@ -1,9 +1,13 @@
 package org.example.logic.useCase
 
-import org.example.logic.models.Project
+import org.example.logic.models.*
 import org.example.logic.repositries.AuthenticationRepository
 import org.example.logic.repositries.ProjectRepository
+import org.example.logic.utils.*
+import kotlin.uuid.ExperimentalUuidApi
+import kotlin.uuid.Uuid
 
+@OptIn(ExperimentalUuidApi::class)
 class CreateStateUseCase(
     private val projectRepository: ProjectRepository,
     private val authenticationRepository: AuthenticationRepository,
@@ -13,6 +17,44 @@ class CreateStateUseCase(
         stateName: String,
         projectId: String,
     ): Project {
-        TODO("Not yet implemented")
+        checkInputValidation(stateName, projectId)
+        checkUserRole()
+        val project = getProject(projectId)
+        val newState =
+            State(
+                id = Uuid.random().toHexString(),
+                title = stateName,
+            )
+        val updatedProject =
+            updateProjectUseCase(
+                project.copy(
+                    states = project.states + newState,
+                ),
+            )
+        return updatedProject
     }
+
+    private fun getProject(projectId: String): Project =
+        (
+            projectRepository.getProjectById(projectId)
+                ?: throw ProjectNotFoundException("Project not found")
+        )
+
+    private fun checkUserRole() {
+        if (getCurrentUser().role != UserRole.ADMIN) throw UnauthorizedException("User is not an admin")
+    }
+
+    private fun checkInputValidation(
+        stateName: String,
+        projectId: String,
+    ) {
+        when {
+            stateName.isBlank() -> throw BlankInputException("State name cannot be blank")
+            projectId.isBlank() -> throw BlankInputException("Project id cannot be blank")
+        }
+    }
+
+    private fun getCurrentUser() =
+        authenticationRepository.getCurrentUser()
+            ?: throw NoLoggedInUserException("No logged in user")
 }
