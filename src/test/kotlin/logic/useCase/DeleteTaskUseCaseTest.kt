3356a4485f7f3e59a761f5bff27730d6109ef1be
am17jx <ameerahmed0780@gmail.com>
2025-04-30 14:20:57 +0300

    Add: Delete Task Use Case

---
+++ src/test/kotlin/logic/useCase/DeleteTaskUseCaseTest.kt
@@ -0,0 +1,75 @@
+import com.google.common.truth.Truth.assertThat
+import io.mockk.mockk
+import org.example.logic.repositries.AuditLogRepository
+import org.example.logic.repositries.AuthenticationRepository
+import org.example.logic.repositries.ProjectRepository
+import org.example.logic.useCase.CreateProjectUseCase
+import org.example.logic.utils.BlankInputException
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+class DeleteTaskUseCaseTest {
+
+    private lateinit var taskRepository: TaskRepository
+    private lateinit var auditLogRepository: AuditLogRepository
+    private lateinit var authenticationRepository: AuthenticationRepository
+    private lateinit var deleteTaskUseCase: DeleteTaskUseCase
+
+    @BeforeEach
+    fun setUp() {
+        taskRepository = mockk()
+        auditLogRepository = mockk()
+        authenticationRepository = mockk()
+        deleteTaskUseCase = DeleteTaskUseCase(taskRepository, auditLogRepository, authenticationRepository)
+    }
+
+    @Test
+    fun `should delete task successfully when user is admin`() {
+        val user = User("admin", "hashedPassword", UserRole.ADMIN)
+        val taskId = "task-001"
+        val projectId = "project-001"
+        val task = Task(taskId, "Test Task", "TODO", projectId)
+
+        every { taskRepository.getTaskById(taskId) } returns task
+        every { taskRepository.deleteTask(taskId) } returns true
+        every { auditLogRepository.logDeleteAction(user, taskId, projectId) } just Runs
+
+        val result = deleteTaskUseCase.execute(user, taskId, projectId)
+
+
+        assertThat(result).isTrue()
+        verify { auditLogRepository.logDeleteAction(user, taskId, projectId) }
+    }
+
+    @Test
+    fun `should throw exception if task not found`() {
+        val user = User("mate", "hashedPassword", UserRole.MATE)
+        val taskId = "task-002"
+        val projectId = "project-001"
+
+        every { taskRepository.getTaskById(taskId) } returns null
+
+        assertThrows<BlankInputException> {
+            deleteTaskUseCase.execute(user, taskId, projectId)
+        }
+    }
+
+    @Test
+    fun `should throw exception if user is not admin and does not have permission to delete task`() {
+        val user = User("mate", "hashedPassword", UserRole.MATE)
+        val taskId = "task-003"
+        val projectId = "project-001"
+        val task = Task(taskId, "Test Task", "TODO", "another-project-id")
+
+
+        every { taskRepository.getTaskById(taskId) } returns task
+
+
+        assertThrows<IllegalArgumentException> {
+            deleteTaskUseCase.execute(user, taskId, projectId)
+        }
+    }
+}
+
+
