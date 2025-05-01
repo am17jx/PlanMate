c4b734917a72f9d7e7f799fe09bfdcdaebbf547b
Mostafa Salah <mostafa.salah.569x@gmail.com>
2025-05-01 09:39:49 +0300

    feat: add UserCsvMapper

---
+++ src/main/kotlin/data/utils/mapper/UserCsvMapper.kt
@@ -0,0 +1,15 @@
+package org.example.data.utils.mapper
+
+import org.example.logic.models.User
+
+fun User.toCsvRow(): String {
+    TODO("Not yet implemented")
+}
+
+fun List<User>.toCsvRows(): List<String> {
+    TODO("Not yet implemented")
+}
+
+fun List<String>.toUsers(): List<User> {
+    TODO("Not yet implemented")
+}
