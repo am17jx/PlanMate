ed188fecbc16d45c5350f6ef6b3bd1ce8ffc82f3
Mostafa Salah <mostafa.salah.569x@gmail.com>
2025-05-01 23:45:00 +0300

    feat: add implementation for UserCsvMapper functions

--- src/main/kotlin/data/utils/mapper/UserCsvMapper.kt
+++ src/main/kotlin/data/utils/mapper/UserCsvMapper.kt
@@ -1,15 +1,28 @@
 package org.example.data.utils.mapper
 
 import org.example.logic.models.User
+import org.example.logic.models.UserRole
 
 fun User.toCsvRow(): String {
-    TODO("Not yet implemented")
+    return "$id,$username,$password,$role"
 }
 
 fun List<User>.toCsvRows(): List<String> {
-    TODO("Not yet implemented")
+    val listOfRows = mutableListOf<String>()
+
+    return this.map {
+        it.toCsvRow()
+    }
+
 }
 
 fun List<String>.toUsers(): List<User> {
-    TODO("Not yet implemented")
+    val usersList = mutableListOf<User>()
+
+    this.forEach { row ->
+        val partsOfUser = row.split(",")
+        usersList.add(User(partsOfUser[0],partsOfUser[1],partsOfUser[2], UserRole.valueOf(partsOfUser[3])))
+    }
+
+    return usersList
 }
