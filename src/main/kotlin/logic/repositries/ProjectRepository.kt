369c3926a50b21ae2a374852bcb51b023ff775d8
Abdallah Bakhet <abdallahM_bakhet@outlook.com>
2025-05-02 00:35:34 +0300

    refactor: ProjectRepository return types

--- src/main/kotlin/logic/repositries/ProjectRepository.kt
+++ src/main/kotlin/logic/repositries/ProjectRepository.kt
@@ -3,13 +3,13 @@ package org.example.logic.repositries
 import org.example.logic.models.Project
 
 interface ProjectRepository {
-    fun createProject(project: Project): Project?
+    fun createProject(project: Project): Project
 
-    fun updateProject(updatedProject: Project): Project
+    fun updateProject(updatedProject: Project): Project?
 
     fun deleteProject(projectId: String)
 
     fun getAllProjects(): List<Project>
 
-    fun getProjectById(projectId: String): Project?
+    fun getProjectById(projectId: String): Project
 }
