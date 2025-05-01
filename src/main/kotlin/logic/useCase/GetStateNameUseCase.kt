d61288c618f9bf165bfbe8e0a61f5d4ff019993b
Mohamed Hesham <medo93346@gmail.com>
2025-05-01 16:20:52 +0300

    refactor: GetStateNameUseCase logic

--- src/main/kotlin/logic/useCase/GetStateNameUseCase.kt
+++ src/main/kotlin/logic/useCase/GetStateNameUseCase.kt
@@ -1,15 +1,20 @@
 package org.example.logic.useCase
 
 import org.example.logic.models.Project
+import org.example.logic.models.State
 import org.example.logic.models.Task
+import org.example.logic.utils.StateNotFoundException
 
 class GetStateNameUseCase(
     private val getTaskByIdUseCase: GetTaskByIdUseCase,
     private val getProjectByIdUseCase: GetProjectByIdUseCase
 ) {
-    operator fun invoke(taskId: String): String? {
+    operator fun invoke(taskId: String): String {
         val task: Task = getTaskByIdUseCase(taskId)
         val project: Project = getProjectByIdUseCase(task.projectId)
-        return project.states.firstOrNull { it.id == task.stateId }?.title
+        return getState(project,task.stateId).title
     }
+    private fun getState(project: Project, stateId: String): State =
+        project.states.find { it.id == stateId }
+            ?: throw StateNotFoundException("State not found")
 }
