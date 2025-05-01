646ddaa2a78161a2275ad3bfbe3bc5763846682a
Sara Ashraf <saraashraf1822004@gmail.com>
2025-05-01 16:29:03 +0300

    feat: Implement AdminHomeUI

--- src/main/kotlin/presentation/MainUiController.kt
+++ src/main/kotlin/presentation/MainUiController.kt
@@ -7,6 +7,7 @@ import org.example.presentation.navigation.Route
 import org.example.presentation.role.Admin
 import org.example.presentation.role.Mate
 import org.example.presentation.role.User
+import org.example.presentation.screens.AdminHomeUI
 import org.example.presentation.screens.LoginUI
 import org.example.presentation.screens.ShowAllProjectsUI
 import org.koin.java.KoinJavaComponent.getKoin
@@ -33,6 +34,17 @@ class MainUiController(
                     },
                     loginUserUseCase = getKoin().get()
                 )
+            }
+
+            is Route.AdminHomeRoute -> {
+                AdminHomeUI { choice ->
+                    when (choice) {
+                        1 -> navigationController.navigateTo(Route.ShowAllProjectsRoute(UserRole.ADMIN))
+                        2 -> println("Create Project - Coming soon!")
+                        3 -> println("Create User - Coming soon!")
+                        4 -> navigationController.popBackStack()
+                    }
+                }
             }
 
             is Route.ShowAllProjectsRoute -> {
--- src/main/kotlin/presentation/navigation/Route.kt
+++ src/main/kotlin/presentation/navigation/Route.kt
@@ -4,5 +4,6 @@ import org.example.logic.models.UserRole
 
 sealed interface Route {
     data object LoginRoute : Route
+    data object AdminHomeRoute : Route
     data class ShowAllProjectsRoute(val userRole: UserRole) : Route
 }
