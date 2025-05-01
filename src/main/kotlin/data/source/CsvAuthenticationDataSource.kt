66a4325678ff9791f2c8bae22eac0da861f0a444
Mostafa Salah <mostafa.salah.569x@gmail.com>
2025-05-01 09:49:47 +0300

    feat: add CsvAuthenticationDataSource class

---
+++ src/main/kotlin/data/source/CsvAuthenticationDataSource.kt
@@ -0,0 +1,21 @@
+package data.source
+
+import data.source.local.contract.LocalAuthenticationDataSource
+import org.example.data.utils.CSVReader
+import org.example.data.utils.CSVWriter
+import org.example.logic.models.User
+import java.io.File
+
+class CsvAuthenticationDataSource(file: File): LocalAuthenticationDataSource {
+
+    private val csvWriter = CSVWriter(file)
+    private val csvReader = CSVReader(file)
+
+    override fun saveUser(user: User) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getAllUsers(): List<User> {
+        TODO("Not yet implemented")
+    }
+}
