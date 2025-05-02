c3e99e46e4397bdf29d6c3e03632e6862df1a241
Mostafa Salah <mostafa.salah.569x@gmail.com>
2025-05-02 09:45:26 +0300

    feat: add CreateMateUseCase in UseCaseModule

--- src/main/kotlin/di/UseCaseModule.kt
+++ src/main/kotlin/di/UseCaseModule.kt
@@ -2,6 +2,7 @@ package di
 
 import logic.useCase.*
 import org.example.logic.useCase.LoginUserUseCase
+import org.example.logic.useCase.CreateMateUseCase
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 
@@ -9,4 +10,5 @@ import org.koin.dsl.module
 val useCaseModule =
     module {
         singleOf(::LoginUserUseCase)
+        singleOf(::CreateMateUseCase)
     }
