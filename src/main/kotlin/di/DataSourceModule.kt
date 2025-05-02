ac58913fb99c5427535fc90039567b35d70e2ea0
Mostafa Salah <mostafa.salah.569x@gmail.com>
2025-05-02 19:02:47 +0300

    fix: solve conflicts with develop

--- src/main/kotlin/di/DataSourceModule.kt
+++ src/main/kotlin/di/DataSourceModule.kt
@@ -8,14 +8,9 @@ import org.example.data.source.local.CsvTaskDataSource
 import org.example.data.source.local.contract.LocalProjectDataSource
 import org.example.data.source.local.contract.LocalTaskDataSource
 import org.example.data.source.local.cotract.LocalAuditLogDataSource
-import data.source.CsvAuthenticationDataSource
-import data.source.local.contract.LocalAuthenticationDataSource
 import org.example.data.utils.CSVReader
 import org.example.data.utils.CSVWriter
-import org.koin.core.module.dsl.bind
-import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
-import org.koin.core.module.dsl.bind
 import java.io.File
 
 val dataSourceModule = module {
--- src/main/kotlin/di/RepositoryModule.kt
+++ src/main/kotlin/di/RepositoryModule.kt
@@ -4,9 +4,7 @@ import org.example.data.repository.AuditLogRepositoryImpl
 import org.example.data.repository.AuthenticationRepositoryImpl
 import org.example.data.repository.ProjectRepositoryImpl
 import org.example.data.repository.TaskRepositoryImpl
-import org.example.data.repository.AuthenticationRepositoryImpl
-import org.example.logic.models.User
-import org.example.logic.models.UserRole
+
 import org.example.logic.repositries.AuditLogRepository
 import org.example.logic.repositries.AuthenticationRepository
 import org.example.logic.repositries.ProjectRepository
@@ -14,13 +12,11 @@ import org.example.logic.repositries.TaskRepository
 import org.koin.core.module.dsl.bind
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
-import java.rmi.Naming.bind
-import java.util.*
 
 val repositoryModule =
     module {
         singleOf(::TaskRepositoryImpl) { bind<TaskRepository>() }
         singleOf(::ProjectRepositoryImpl) { bind<ProjectRepository>() }
         singleOf(::AuditLogRepositoryImpl) { bind<AuditLogRepository>() }
-        single <AuthenticationRepository> { AuthenticationRepositoryImpl(get()) }
+        singleOf(::AuthenticationRepositoryImpl) { bind<AuthenticationRepository>()}
     }
