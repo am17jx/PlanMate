aea6045af1e2fab80d443b1f1a7594d981139935
edrees <muhammed.edrees101@gmail.com>
2025-05-02 16:19:14 +0300

    feat: update koin dependencies

--- src/main/kotlin/di/UseCaseModule.kt
+++ src/main/kotlin/di/UseCaseModule.kt
@@ -1,6 +1,8 @@
 package di
 
 import logic.useCase.*
+import org.example.logic.useCase.GetProjectByIdUseCase
+import org.example.logic.useCase.GetProjectTasksUseCase
 import org.example.logic.useCase.GetAllProjectsUseCase
 import org.example.logic.useCase.LoginUserUseCase
 import org.koin.core.module.dsl.singleOf
@@ -12,4 +14,6 @@ val useCaseModule =
         singleOf(::LoginUserUseCase)
         singleOf(::CreateTaskUseCase)
         singleOf(::GetAllProjectsUseCase)
+        singleOf(::GetProjectTasksUseCase)
+        singleOf(::GetProjectByIdUseCase)
     }
