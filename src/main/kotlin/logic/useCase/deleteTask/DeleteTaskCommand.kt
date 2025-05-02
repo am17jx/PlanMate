0b397cbd9f20d472f30a0bf839916035dc9205fb
marah1024 <maroohdroid@gmail.com>
2025-05-02 17:56:14 +0100

    fix : update project & create project with tests

--- src/main/kotlin/logic/useCase/deleteTask/DeleteTaskCommand.kt
+++ src/main/kotlin/logic/useCase/deleteTask/DeleteTaskCommand.kt
@@ -1,24 +1,18 @@
-package org.example.logic.command
+package org.example.logic.useCase.deleteTask
 
+import org.example.logic.command.Command
 import org.example.logic.models.Task
 import org.example.logic.repositries.TaskRepository
 
-
-class TaskDeleteCommand(
+class DeleteTaskCommand(
     private val taskRepository: TaskRepository,
     private val task: Task,
 ) : Command {
-
-    private var deletedTask: Task? = null
-
     override fun execute() {
-        deletedTask = task
         taskRepository.deleteTask(task.id)
-
     }
 
     override fun undo() {
-        deletedTask?.let { taskRepository.createTask(it) }
+        taskRepository.createTask(task)
     }
-
 }
