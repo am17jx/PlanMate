d3a2b5777331f6655f013d52de751d7e81438de5
Abdallah Bakhet <abdallahM_bakhet@outlook.com>
2025-05-09 05:59:40 +0300

    refactor: AppModule Use singleOf for RoleValidationInterceptor

--- src/main/kotlin/di/AppModule.kt
+++ src/main/kotlin/di/AppModule.kt
@@ -2,10 +2,11 @@ package di
 
 import org.example.data.source.remote.RoleValidationInterceptor
 import org.example.di.dataSourceModule
+import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 
 val appModule =
     module {
         includes(uiModule, repositoryModule, useCaseModule, dataSourceModule)
-        single { ::RoleValidationInterceptor }
+        singleOf(::RoleValidationInterceptor)
     }
