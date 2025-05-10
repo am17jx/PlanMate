172f86d668707d16a62ee06e3cabf549b8a48593
marah1024 <maroohdroid@gmail.com>
2025-05-10 16:17:20 +0100

    refactor: update Project use case

--- src/main/kotlin/logic/useCase/UpdateProjectUseCase.kt
+++ src/main/kotlin/logic/useCase/UpdateProjectUseCase.kt
@@ -27,61 +27,14 @@ class UpdateProjectUseCase(
     }
 
     private suspend fun saveUpdatedProject(originalProject: Project, newProject: Project, currentUser: User): Project {
-        val actionBuilder = actionBuilder(originalProject, newProject, currentUser)
-        val auditLog = createAuditLogInstance(originalProject, currentUser, actionBuilder.first)
-
-        auditLogRepository.createAuditLog(auditLog)
 
         projectRepository.updateProject(newProject)
         return newProject
 
     }
 
-    private fun createAuditLogInstance(
-        project: Project,currentUser: User, action: String
-    ): AuditLog {
-
-        val auditLog = AuditLog(
-            id = UUID.randomUUID().toString(),
-            userId = currentUser.id,
-            action = action,
-            timestamp = System.currentTimeMillis(),
-            entityType = AuditLogEntityType.PROJECT,
-            entityId = project.id,
-            actionType = AuditLogActionType.UPDATE
-        )
-        return auditLog
-    }
-
-    private fun actionBuilder(project: Project, updatedProject: Project, currentUser: User): Pair<String, String> {
-        var deletedStateId = ""
-        val timestampNow = Clock.System.now()
-        val action = when {
-            project.name != updatedProject.name -> "${currentUser.username} changed Project name from ${project.name} to ${updatedProject.name}"
-
-            project.states.size > updatedProject.states.size -> {
-                deletedStateId = project.states.subtract(updatedProject.states.toSet()).first().id
-                "${currentUser.username} deleted a state: ${
-                    project.states.subtract(updatedProject.states.toSet()).first().title
-                } from ${project.name}"
-            }
-
-            project.states.size < updatedProject.states.size -> "${currentUser.username} add  state: ${
-                updatedProject.states.subtract(project.states.toSet()).first().title
-            } to ${project.name}"
-
-            else -> {
-                val statesDifference =
-                    (updatedProject.states.subtract(project.states.toSet()) + project.states.subtract(updatedProject.states.toSet())).toList()
-                "${currentUser.username} updated state : ${statesDifference[1].title} to state:${statesDifference[0].title} in ${project.name}"
-            }
-
-        }
-        return Pair(action + "at ${timestampNow.formattedString()}", deletedStateId)
-    }
-
     private fun detectChanges(originalProject: Project, newProject: Project) {
-        if ((originalProject.name == newProject.name) && (originalProject.states.toSet() == newProject.states.toSet())) throw ProjectNotChangedException(
+        if ((originalProject.name == newProject.name) && (originalProject.tasksStatesIds.toSet() == newProject.tasksStatesIds.toSet())) throw ProjectNotChangedException(
             "No changes detected ^_^"
         )
     }
