b302ce8ed3eed958855ed0337b13f97a52ca5cee
Mostafa Salah <mostafa.salah.569x@gmail.com>
2025-05-10 12:28:20 +0300

    refactor: delete instant utils and move formattedString extension function to Extensions file

--- src/main/kotlin/logic/utils/Extensions.kt
+++ src/main/kotlin/logic/utils/Extensions.kt
@@ -1,5 +1,13 @@
 package org.example.logic.utils
 
+import kotlinx.datetime.Instant
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.format
+import kotlinx.datetime.format.DateTimeComponents
+import kotlinx.datetime.format.DateTimeFormatBuilder
+import kotlinx.datetime.format.Padding
+import kotlinx.datetime.format.char
+import kotlinx.datetime.toLocalDateTime
 import org.example.logic.utils.Constants.MAX_ID_LENGTH
 import kotlin.uuid.ExperimentalUuidApi
 import kotlin.uuid.Uuid
@@ -8,3 +16,17 @@ import kotlin.uuid.Uuid
 fun Uuid.getCroppedId(limit: Int = MAX_ID_LENGTH): String = this.toHexString().substring(0, limit)
 
 fun String.isValidId() = matches(Regex("^[a-zA-Z0-9]+$")) && length == MAX_ID_LENGTH
+
+fun Instant.formattedString(): String{
+    val customFormat = DateTimeComponents.Format {
+        year(); char('/'); monthNumber(padding = Padding.ZERO); char('/'); dayOfMonth(padding = Padding.ZERO)
+        char(' ')
+        hour(Padding.ZERO); char(':'); minute(padding = Padding.ZERO); char(' ')
+        amPm(this@formattedString.toLocalDateTime(TimeZone.currentSystemDefault()).hour)
+    }
+    return this.format(customFormat)
+}
+
+fun DateTimeFormatBuilder.WithDateTimeComponents.amPm(hour: Int) {
+    chars(if (hour < 12) "AM" else "PM")
+}
