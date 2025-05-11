91754ec9b5d120e7dd6b5be1c8b6fbdca6478bf3
Sara Ashraf <saraashraf1822004@gmail.com>
2025-05-11 16:31:20 +0300

    refactor: rename GetProjectStatesUseCase to GetProjectStateUseCase

--- src/main/kotlin/logic/useCase/GetProjectStateUseCase.kt
+++ src/main/kotlin/logic/useCase/GetProjectStateUseCase.kt
@@ -2,15 +2,14 @@ package org.example.logic.useCase
 
 import org.example.logic.models.Project
 import org.example.logic.models.State
-import org.example.logic.models.Task
-import org.example.logic.repositries.TaskStateRepository
+import org.example.logic.repositries.ProjectStateRepository
 
-class GetProjectStatesUseCase(
+class GetProjectStateUseCase(
     private val getProjectByIdUseCase: GetProjectByIdUseCase,
-    private val taskStateRepository: TaskStateRepository
+    private val projectStateRepository: ProjectStateRepository
 ) {
     suspend operator fun invoke(projectId: String): List<State> {
         val project: Project = getProjectByIdUseCase(projectId)
-        return taskStateRepository.getProjectTaskStates(project.tasksStatesIds)
+        return projectStateRepository.getProjectTaskStates(project.projectStateIds)
     }
 }
