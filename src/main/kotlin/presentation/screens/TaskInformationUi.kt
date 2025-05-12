bdab9ffa61c40279b4181ba0e726eca91ff7db64
Abdallah Bakhet <abdallahM_bakhet@outlook.com>
2025-05-12 13:13:54 +0300

    fix: navigation back issue

--- src/main/kotlin/presentation/screens/TaskInformationUi.kt
+++ src/main/kotlin/presentation/screens/TaskInformationUi.kt
@@ -48,9 +48,7 @@ class TaskInformationUi(
                         }
 
                         "3" -> showTaskLogs(taskId)
-                        "4" -> {
-                            onNavigateBack
-                        }
+                        "4" -> onNavigateBack()
 
                         else -> viewer.display("Invalid choice. Please try again.")
                     }
@@ -123,7 +121,7 @@ class TaskInformationUi(
 
             viewer.display("Select a new state index:")
             val index = reader.readInt()
-            val newState  =
+            val newState =
                 if (index == null || index !in 1..stateIds.size) {
                     viewer.display("Invalid index, keeping old state.")
                     ProjectState(id = task.stateId, title = task.stateName, projectId = task.projectId)
@@ -190,10 +188,8 @@ class TaskInformationUi(
         }
 
     companion object {
-        fun create(
-            onNavigateBack: () -> Unit
-        ): TaskInformationUi {
-            return TaskInformationUi(
+        fun create(onNavigateBack: () -> Unit): TaskInformationUi =
+            TaskInformationUi(
                 getTaskByIdUseCase = getKoin().get(),
                 onNavigateBack = onNavigateBack,
                 getStateNameUseCase = getKoin().get(),
@@ -206,6 +202,5 @@ class TaskInformationUi(
                 tablePrinter = getKoin().get(),
                 getProjectStatesUseCase = getKoin().get(),
             )
-        }
     }
 }
